431fef0b3b461dd3b1350ce8b279ff45388ca154
Erik Zenker <erikzenker@posteo.de>
2020-05-04 16:52:15 +0200

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,35 @@
+from conans import ConanFile, CMake, tools
+
+
+class HsmConan(ConanFile):
+    name = "hsm"
+    version = "1.0"
+    license = "MIT"
+    author = "Erik Zenker erikzenker@hotmail.com"
+    url = "https://github.com/erikzenker/hsm.git"
+    description = "The hana state machine (hsm) is a finite state machine library based on the boost hana meta programming library. It follows the principles of the boost msm and boost sml libraries, but tries to reduce own complex meta programming code to a minimum."
+    topics = ("state machine", "template meta programming")
+    requires = "boost/1.72.0"
+    no_copy_source = True
+    generators = "cmake"
+
+    def source(self):
+        self.run("git clone https://github.com/erikzenker/hsm.git")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="hsm")
+        cmake.build()
+        cmake.test()
+
+    def package(self):
+        self.copy("*.h", dst="include", src="hsm/src/include")
+
+    def package_id(self):
+        self.info.header_only()
+
+    def package_info(self):
+        self.info.header_only()
+        self.cpp_info.libdirs = []
+        self.cpp_info.libs = ["hsm"]
+
---
+++ test_package/example.cpp
@@ -0,0 +1,66 @@
+#include <hsm/hsm.h>
+
+#include <cassert>
+
+// States
+struct Locked {
+};
+struct Unlocked {
+};
+
+// Events
+struct Push {
+};
+struct Coin {
+};
+
+// Guards
+const auto noError = [](auto /*event*/, auto /*source*/, auto /*target*/) { return true; };
+
+// Actions
+const auto beep
+    = [](auto /*event*/, auto /*source*/, auto /*target*/) { std::cout << "beep!" << std::endl; };
+const auto blink = [](auto /*event*/, auto /*source*/, auto /*target*/) {
+    std::cout << "blink, blink, blink!" << std::endl;
+};
+
+struct Turnstile {
+    static constexpr auto make_transition_table()
+    {
+        // clang-format off
+        return hsm::transition_table(
+            // Source               + Event               [Guard]   / Action = Target
+            // +--------------------+---------------------+---------+--------+------------------------+
+            hsm::state<Locked> {}   + hsm::event<Push> {}           / beep   = hsm::state<Locked> {}  ,
+            hsm::state<Locked> {}   + hsm::event<Coin> {} [noError] / blink  = hsm::state<Unlocked> {},
+            // +--------------------+---------------------+---------+--------+------------------------+
+            hsm::state<Unlocked> {} + hsm::event<Push> {} [noError]          = hsm::state<Locked> {}  ,
+            hsm::state<Unlocked> {} + hsm::event<Coin> {}           / blink  = hsm::state<Unlocked> {}
+            // +--------------------+---------------------+---------+--------+------------------------+                        
+            );
+        // clang-format on
+    }
+
+    static constexpr auto initial_state()
+    {
+        return hsm::initial(hsm::state<Locked> {});
+    }
+};
+
+int main()
+{
+    hsm::sm<Turnstile> turnstileSm;
+
+    // The turnstile is initially locked
+    assert(turnstileSm.is(hsm::state<Locked> {}));
+
+    // Inserting a coin unlocks it
+    turnstileSm.process_event(Coin {});
+    assert(turnstileSm.is(hsm::state<Unlocked> {}));
+
+    // Entering the turnstile will lock it again
+    turnstileSm.process_event(Push {});
+    assert(turnstileSm.is(hsm::state<Locked> {}));
+
+    return 0;
+}
